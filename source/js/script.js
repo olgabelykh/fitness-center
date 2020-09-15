7bcdadb0c51cd0d55065e3940a4edc09a340652f
olya belykh <belykh.o.v@gmail.com>
2020-09-15 12:09:01 +0300

    Исправлен баг отображения картинок на iphone

--- source/js/script.js
+++ source/js/script.js
@@ -1,33 +1,40 @@
-"use strict";
+'use strict';
 
 (function () {
   var instructorsSlider = document
-    .querySelector("#instructors")
-    .querySelector(".swiper-container");
+    .querySelector('#instructors')
+    .querySelector('.swiper-container');
 
   if (!instructorsSlider) {
     return;
   }
 
   var instructorsSwiper = new Swiper(instructorsSlider, {
-    slidesPerView: "auto",
-    spaceBetween: 40,
+    slidesPerView: 1,
+    spaceBetween: 0,
     loopedSlides: 8,
     loop: true,
     loopFillGroupWithBlank: true,
     updateOnWindowResize: true,
 
     navigation: {
-      nextEl: ".slider--instructors .slider__button--next",
-      prevEl: ".slider--instructors .slider__button--prev",
+      nextEl: '.slider--instructors .slider__button--next',
+      prevEl: '.slider--instructors .slider__button--prev',
+    },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+      },
     },
   });
 })();
 
 (function () {
   var reviewsSlider = document
-    .querySelector("#reviews")
-    .querySelector(".swiper-container");
+    .querySelector('#reviews')
+    .querySelector('.swiper-container');
 
   if (!reviewsSlider) {
     return;
@@ -41,37 +48,37 @@
     updateOnWindowResize: true,
 
     navigation: {
-      nextEl: ".slider--reviews .slider__button--next",
-      prevEl: ".slider--reviews .slider__button--prev",
+      nextEl: '.slider--reviews .slider__button--next',
+      prevEl: '.slider--reviews .slider__button--prev',
     },
   });
 })();
 
 (function () {
-  var membershipTabs = document.querySelector("#membership-tabs");
+  var membershipTabs = document.querySelector('#membership-tabs');
 
   if (!membershipTabs) {
     return;
   }
 
-  membershipTabs.querySelectorAll(".membership__tab-item").forEach(function (item) {
-    item.addEventListener("click", function (event) {
+  membershipTabs.querySelectorAll('.membership__tab-item').forEach(function (item) {
+    item.addEventListener('click', function (event) {
       event.preventDefault();
 
-      var activeTab = membershipTabs.querySelector(".membership__tab-item--active");
+      var activeTab = membershipTabs.querySelector('.membership__tab-item--active');
       if (item.isSameNode(activeTab)) {
         return;
       }
-      activeTab.classList.remove("membership__tab-item--active");
-      item.classList.add("membership__tab-item--active");
+      activeTab.classList.remove('membership__tab-item--active');
+      item.classList.add('membership__tab-item--active');
 
-      var activeListId = activeTab.querySelector("a").getAttribute("href");
-      var newActiveListId = item.querySelector("a").getAttribute("href");
+      var activeListId = activeTab.querySelector('a').getAttribute('href');
+      var newActiveListId = item.querySelector('a').getAttribute('href');
 
       var activeList = document.querySelector(activeListId);
-      activeList.classList.remove("membership__list--active");
+      activeList.classList.remove('membership__list--active');
       var newActiveList = document.querySelector(newActiveListId);
-      newActiveList.classList.add("membership__list--active");
+      newActiveList.classList.add('membership__list--active');
     });
   });
 })();
